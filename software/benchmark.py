44a50bc1c6b8868c6d20e14663ce540eac452ff9
MarcDcls <marc.duclusaud@gmail.com>
2026-01-21 19:18:11 +0100

    Benchmark control

--- software/benchmark.py
+++ software/benchmark.py
@@ -13,18 +13,30 @@ import numpy as np
 import time
 
 DT = 0.01
+JOINT_GAINS = {"knee_motor": -1.0, "ankle_motor": 1.0}
 
 robot = placo.RobotWrapper("hardware/benchmark")
 solver = placo.KinematicsSolver(robot)
 
-# Timing belt system
-gear_task = solver.add_gear_task()
-gear_task.add_gear("knee_motor", "knee_joint", 1.0)
-gear_task.add_gear("ankle_motor", "ankle_passive", 1.0)
-gear_task.add_gear("ankle_passive", "ankle_joint", 1.0)
+# Knee timing belt
+knee_gear_task = solver.add_gear_task()
+knee_gear_task.add_gear("knee_joint", "knee_motor", -48/24)
+knee_gear_task.configure("knee", "hard", 1.0)
 
-# Closing kinematic loop
-solver.add_distance_task("closing_crank", "closing_foot", 0.0).configure("closing", "hard", 1.0)
+# Ankle timing belt
+ankle_gear_task = solver.add_gear_task()
+ankle_gear_task.add_gear("ankle_passive", "ankle_motor", 48/24)
+ankle_gear_task.add_gear("ankle_passive", "knee_joint", -1.0)
+ankle_gear_task.configure("ankle", "hard", 1.0)
+
+# Closing crank kinematic loop
+closing_task = solver.add_relative_position_task("closing_crank", "closing_foot", np.zeros(3))
+closing_task.mask.set_axises("xz")
+closing_task.configure("closing", "hard", 1.0)
+
+# Controlling in joint space
+joint_task = solver.add_joints_task()
+joint_task.set_joints({"knee_joint": 0.0, "ankle_joint": 0.0})
 
 solver.add_regularization_task(1e-6)
 
@@ -39,8 +51,21 @@ t = 0.0
 while True:
     step_start = time.time()      
 
-    robot.set_joint("ankle_motor", np.sin(2*t) * 0.5)
+    # Knee oscillation
+    knee_target = np.sin(2*t - np.pi/2) * 0.5 + 0.5
+    ankle_target = 0.0
+
+    # Ankle oscillation
+    # knee_target = 0.0
+    # ankle_target = np.sin(2*t) * 0.5
+
+    joint_task.set_joints({
+        "knee_joint": JOINT_GAINS["knee_motor"] * knee_target,
+        "ankle_joint": JOINT_GAINS["ankle_motor"] * ankle_target
+    })
+
     solver.solve(True)
+    solver.dump_status()
     robot.update_kinematics()
     
     viz.display(robot.state.q)
