afc42629aad062e818011daa30254ba316cb6f57
MarcDcls <marc.duclusaud@gmail.com>
2026-01-28 18:46:31 +0100

    Adding scripts to root

---
+++ read_rom.sh
@@ -0,0 +1,14 @@
+# Copyright 2026 Marc Duclusaud
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <motor_ids>"
+    exit 1
+fi
+
+uv run python software/read_rom.py "$@"
---
+++ set_acceleration.sh
@@ -0,0 +1,14 @@
+# Copyright 2026 Marc Duclusaud
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at:
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+if [ "$#" -lt 2 ]; then
+    echo "Usage: $0 <acceleration_value> <motor_ids>"
+    exit 1
+fi
+
+uv run python software/set_acceleration.py "$@"
